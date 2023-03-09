4e2257816c4c5d46e922401d16bd7ba1502b3ee6
Zahar Parhomchuk <z.parhomchuk@outlook.com>
2023-03-09 03:57:51 +0300

    Add test  ciliates highliter

---
+++ funcs.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+
+ITERATE_LIMIT = 10
+iterate_counter  = 0
+ciliate = []
+
+def get_diff(act, buf, counter, buf_size):
+    (h, w) = act.shape[0:2]
+    diff = np.copy(act)
+    counter = 0
+    for i in range(counter, counter + buf_size):
+        if (i - counter) % 5 == 0:
+            continue
+        pre_frame = buf[i % buf_size]
+        for y in range(0, h):
+            for x in range(0, w):
+                if act[y, x] != pre_frame[y, x]:
+                    counter = counter + 1
+                if act[y, x] == 255 and pre_frame[y, x] == 255:
+                    diff[y, x] = 0    
+
+    print(counter)
+    return diff
+
+def recognize_ciliates(frame, diff_frame):
+    (h, w) = frame.shape[0:2]
+    rects = []
+    for y in range(0, h):
+            for x in range(0, w):
+                if diff_frame[y, x] == 255:
+                    rect = get_ciliate(frame, y, x)
+                    del_rect(diff_frame, rect)
+                    rects.append(rect)
+    return rects
+
+
+def get_ciliate(frame, y, x):
+    global iterate_counter
+    global ciliate
+    iterate_counter = 1
+    ciliate = []
+    get_ciliate_rec(frame, y, x)
+    top_left = [y, x]
+    bottom_right = [y, x]
+    for pix in ciliate:
+        if pix[0] <= top_left[0] and pix[1] <= top_left[1]:
+            top_left = pix
+        if pix[0] >= bottom_right[0] and pix[1] >= bottom_right[1]:
+            bottom_right = pix
+    return [top_left, bottom_right]
+
+    
+
+def get_ciliate_rec(frame, y, x):
+    global iterate_counter
+    global ciliate
+    iterate_counter = iterate_counter + 1
+    ciliate.append([y, x])
+    if iterate_counter == ITERATE_LIMIT:
+        return
+    # выход за границы изображения
+    for xi in range(x-1, x+1):
+        for yi in range(y-1, y+1):
+            if frame[yi, xi] == 255 and not([yi, xi] in ciliate):
+                get_ciliate_rec(frame, yi, xi)
+
+    return
+    
+def del_rect(frame, rect):
+    for y in range(rect[0][0], rect[1][0]):
+        for x in range(rect[0][1], rect[1][1]):
+            frame[y, x] = 0
+
+def highligth(frame, rects):
+    for rect in rects:
+        for y in range(rect[0][0]-2, rect[1][0]+2):
+            l = rect[0][1]- 2
+            r = rect[1][1]+2
+            try:
+                frame[y, l] = (255, 0, 0)
+                frame[y, r] = (255, 0, 0)
+            except:
+                continue
+        for x in range(rect[0][1]-2, rect[1][1]+2):
+            t = rect[0][0]-2
+            b = rect[1][0]+2
+            try:
+                frame[t, x] = (255, 0, 0)
+                frame[b, x] = (255, 0, 0)
+            except:
+                continue
+
+    return frame
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,30 +1,85 @@
 import cv2
-
 import numpy as np
+import funcs
+
+#-----------------------
+# Кювета  в кадре [100:1050, 500:1200]
+# Участок соединенения двух объемов [450:650, 800:950]
+#-----------------------
 
 cap = cv2.VideoCapture("videos/original_20s.mov")
+# cap = cv2.VideoCapture(0)
+
 # ret, frame = cap.read()
 # (h, w) = frame.shape[:2]
 # fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # out = cv2.VideoWriter('temporary/output.mov', fourcc, 20.0, (h, w))
-cv2.namedWindow("modified", cv2.WINDOW_FREERATIO)
-cv2.namedWindow("origin", cv2.WINDOW_FREERATIO)
+cv2.namedWindow("WinOrigin", cv2.WINDOW_FREERATIO)
+cv2.namedWindow("WinModified", cv2.WINDOW_FREERATIO)
+cv2.namedWindow("WinDiff", cv2.WINDOW_FREERATIO)
+
+frame_buff = []
+counter = 0
+BUFF_SIZE = 30
+buff_ready = False
 
 scale = 0.8
+i = 0
+
+# Для теста фильтров
+# ret = False
+# while True:
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         ret, frame = cap.read()
+#         if not ret:
+#             break  
+#         frame = frame[590:700, 800:950]
+        
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     if ret:
+#         cv2.imshow("WinOrigin", frame)
+#         cv2.imshow("WinModified", gray)   
+
 while True:
     ret, frame = cap.read()
     if not ret:
-        break
+        break   
+
+    frame = frame[590:700, 800:950]
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # out.write(frame)
 
-    gray_croped = gray[450:650, 800:950]
+    gray_croped = gray
+    T, act_mod = cv2.threshold(gray_croped, 127, 255, cv2.THRESH_BINARY) 
 
-    cv2.imshow('origin', frame)
-    cv2.imshow('modified', gray_croped)   
+    if not buff_ready:
+        frame_buff.append(np.copy(act_mod))
+        counter = counter + 1
+        if counter == BUFF_SIZE:
+            buff_ready = True
+            counter = 0
+    else:
+        img_diff = funcs.get_diff(act_mod, frame_buff, counter, BUFF_SIZE)
 
-    img = cv2.resize(gray_croped, (gray_croped.shape[0]*10, gray_croped.shape[1]*10), interpolation= cv2.INTER_LINEAR)
-    cv2.imwrite("temporary/gray_frame.jpeg", img) 
+        cv2.imshow("WinOrigin", act_mod)
+        cv2.imshow("WinDiff", img_diff)
+
+        ciliates = funcs.recognize_ciliates(act_mod, img_diff)
+
+        frame_mod = funcs.highligth(frame, ciliates)
+        cv2.imshow("WinModified", frame_mod)
+
+        frame_buff[counter] = np.copy(act_mod)
+        counter = counter + 1
+
+        if counter == BUFF_SIZE:
+            counter = 0
+
+
+    # img = cv2.resize(gray_croped, (gray_croped.shape[0]*10, gray_croped.shape[1]*10), interpolation= cv2.INTER_LINEAR)
+
+
+    # cv2.imwrite("temporary/bin_frame" +str(i)+ ".jpeg", act_mod) 
+    # cv2.imwrite("temporary/gray_frame" +str(i)+ ".jpeg", gray_croped)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
